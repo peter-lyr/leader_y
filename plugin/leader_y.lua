a00236d9113adede8e936c9a10592d841939af45
peter-lyr <llydrp.ldp@gmail.com>
2024-09-26 00:22:59 +0800

    .

--- plugin/leader_y.lua
+++ plugin/leader_y.lua
@@ -4,11 +4,14 @@ require 'which-key'.register {
   ['<leader>y'] = { name = 'yank', },
   ['<leader>yf'] = { name = 'yank.file', },
   ['<leader>yr'] = { name = 'yank.file.relative', },
+
   ['<leader>yff'] = { function() F.yank_clipbaord_file_full() end, 'yank_clipbaord_file_full', mode = { 'n', 'v', }, },
   ['<leader>yfh'] = { function() F.yank_clipbaord_file_full_head() end, 'yank_clipbaord_file_full_head', mode = { 'n', 'v', }, },
   ['<leader>yft'] = { function() F.yank_clipbaord_file_full_tail() end, 'yank_clipbaord_file_full_tail', mode = { 'n', 'v', }, },
+
   ['<leader>yrb'] = { function() F.yank_clipbaord_file_bufname() end, 'yank_clipbaord_file_bufname', mode = { 'n', 'v', }, },
   ['<leader>yrh'] = { function() F.yank_clipbaord_file_bufname_head() end, 'yank_clipbaord_file_bufname_head', mode = { 'n', 'v', }, },
   ['<leader>yrt'] = { function() F.yank_clipbaord_file_bufname_tail() end, 'yank_clipbaord_file_bufname_tail', mode = { 'n', 'v', }, },
+
   ['<leader>yw'] = { function() F.yank_clipbaord_cwd() end, 'yank_clipbaord_cwd', mode = { 'n', 'v', }, },
 }
