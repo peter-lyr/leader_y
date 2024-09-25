931aaa8c0abc556d495451816f10547ebd691c1f
peter-lyr <llydrp.ldp@gmail.com>
2024-09-26 00:17:47 +0800

    <leader>yrh yank_clipbaord_file_full

--- plugin/leader_y.lua
+++ plugin/leader_y.lua
@@ -4,5 +4,8 @@ require 'which-key'.register {
   ['<leader>y'] = { name = 'yank', },
   ['<leader>yf'] = { name = 'yank.file', },
   ['<leader>yff'] = { function() F.yank_clipbaord_file_full() end, 'yank_clipbaord_file_full', mode = { 'n', 'v', }, },
+  ['<leader>yrb'] = { function() F.yank_clipbaord_file_bufname() end, 'yank_clipbaord_file_bufname', mode = { 'n', 'v', }, },
+  ['<leader>yrh'] = { function() F.yank_clipbaord_file_bufname_head() end, 'yank_clipbaord_file_bufname_head', mode = { 'n', 'v', }, },
+  ['<leader>yrt'] = { function() F.yank_clipbaord_file_bufname_tail() end, 'yank_clipbaord_file_bufname_tail', mode = { 'n', 'v', }, },
   ['<leader>yw'] = { function() F.yank_clipbaord_cwd() end, 'yank_clipbaord_cwd', mode = { 'n', 'v', }, },
 }
